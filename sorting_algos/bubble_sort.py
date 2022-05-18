621455cc7a0861049b88539e87ed36bbba671dd1
PeepoM <peter.marks@outlook.sk>
2022-05-18 20:35:23 +0200

    implemented bubble sort

---
+++ sorting_algos/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+if __name__ == "__main__":
+    arr = [5, 1, 8, 2, 0, 9, 3, 10, 4, 6, 7]
+    bubble_sort(arr)
+    print(arr)
